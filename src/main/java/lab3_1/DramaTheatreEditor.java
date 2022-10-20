7d467e342325228d9004e8077120b5646d540698
AquaUseful <l.vladik.m@gmail.com>
2022-10-20 21:23:34 +0300

    Remove unused types and exceptions

--- src/main/java/lab3_1/DramaTheatreEditor.java
+++ src/main/java/lab3_1/DramaTheatreEditor.java
@@ -1,8 +1,8 @@
 package lab3_1;
 
-public class DramaTheatreEditor extends TheatreEditor<DramaTheatre> {
+public class DramaTheatreEditor extends TheatreEditor {
     @Override
-    protected void updateObject() throws TheatreException {
+    protected void updateObject() {
         this.obj = new DramaTheatre((String) this.fields.get(0).getValue(),
                 (int) this.fields.get(1).getValue(),
                 (String) this.fields.get(2).getValue());
