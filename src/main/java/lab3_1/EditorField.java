91d935697f28b4e277b7b5ce611cd588fb24f04d
AquaUseful <l.vladik.m@gmail.com>
2022-10-20 21:25:19 +0300

    Add value setter

--- src/main/java/lab3_1/EditorField.java
+++ src/main/java/lab3_1/EditorField.java
@@ -1,6 +1,5 @@
 package lab3_1;
 
-import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
@@ -24,12 +23,16 @@ public abstract class EditorField<T> {
         return this.value;
     }
 
+    public void setValue(T value) {
+        this.value = value;
+    }
+
     private void printPrompt(PrintStream out) {
         out.print("Введите значение поля '" + this.getName() + "': ");
     }
 
     private void printValue(PrintStream out) {
-        out.print("Текущее значение поля '" + this.getName() + "': " + this.getValue());
+        out.println("Текущее значение поля '" + this.getName() + "': " + this.getValue());
     }
 
     public void requestNew(PrintStream out, Scanner scn) {
